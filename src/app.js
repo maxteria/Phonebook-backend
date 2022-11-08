b9c41d65593e33c7a00ab901fcbcf43d99d87677
Maximiliano Morales <maxteriaok@gmail.com>
2022-11-08 12:19:47 -0300

    ADD CORS module

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import { generateId } from "../utils/generateId.js";
 import { PORT } from "./config.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
